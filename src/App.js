07134648ce1487d139e086af709c3d23d2f5e3e3
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-10-28 13:19:49 -0700

    App.js: created addItem and toggleCompleted functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+// - `<App />` will hold all the data needed for this project. It will also be the container for your Todo Components.
+//   - All of your application data will be stored here on `<App />`.
+//   - All of your `handler` functions should live here on `<App />`.
+// you will need a place to store your state in this component.
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+
+import React from "react";
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  addTodo = newTodo => {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
+
+  toggleCompleted = id => {
+    this.setState({
+      todos: this.state.todos.map(item => {
+        if (item.id === id) {
+          return {...item,completed: !item.purchased};
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+
+  filterCompleted = () => {};
+
   render() {
     return (
       <div>
